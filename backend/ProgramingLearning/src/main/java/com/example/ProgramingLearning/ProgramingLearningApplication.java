e9fde37826eecdab51f5458133230bf800904c85
Joan2k2 <joanruizmartinez@gmail.com>
2024-04-21 10:51:00 +0200

    pequeños cambios en la interfaz, y preparo el camino para el backend

---
+++ backend/ProgramingLearning/src/main/java/com/example/ProgramingLearning/ProgramingLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgramingLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramingLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramingLearningApplication.class, args);
+	}
+
+}
---
+++ backend/ProgramingLearning/src/test/java/com/example/ProgramingLearning/ProgramingLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgramingLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramingLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
